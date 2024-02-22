42f7c8488b459a984e414ddcba5e4f632a62dec5
emirbalic <emirbalic@gmail.com>
2024-02-22 18:41:17 +0100

    first commit

---
+++ src/BiddingService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Contracts;
+
+namespace BiddingService;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+
+        CreateMap<Bid, BidDto>();
+        CreateMap<Bid, BidPlaced>();
+        
+    }
+}
