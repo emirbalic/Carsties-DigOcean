1c5f70f1b0e13910d851ce239eae8de2c95a6a29
emirbalic <emirbalic@gmail.com>
2024-02-26 15:03:50 +0100

    Id Workflow AddAgain

--- src/IdentityService/Program.cs
+++ src/IdentityService/Program.cs
@@ -13,9 +13,6 @@ try
 {
     var builder = WebApplication.CreateBuilder(args);
 
-    Log.Information("--> Builder Created!");
-
-
     builder.Host.UseSerilog((ctx, lc) => lc
         .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
         .Enrich.FromLogContext()
