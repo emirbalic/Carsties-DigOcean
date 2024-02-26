326df38e2d1a9d6ae06c1413adf6c4cbd63d16f2
emirbalic <emirbalic@gmail.com>
2024-02-26 15:20:35 +0100

    IdRun

--- src/IdentityService/Program.cs
+++ src/IdentityService/Program.cs
@@ -13,6 +13,9 @@ try
 {
     var builder = WebApplication.CreateBuilder(args);
 
+    Log.Information("--> Builder Created!");
+
+
     builder.Host.UseSerilog((ctx, lc) => lc
         .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
         .Enrich.FromLogContext()
@@ -31,7 +34,7 @@ try
 
     retryPolicy.ExecuteAndCapture(() => SeedData.EnsureSeedData(app));
 
-    
+    Log.Information("--> Retry Policy Executed!");
 
     app.Run();
 }
